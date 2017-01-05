b365a2f83694d308b0738ee14135cbbca71a20b6
r3t2 <ravitejac@gmail.com>
2017-01-04 22:20:55 -0800

    compilatio fix + main

--- algos/DepthFirstOrder.java
+++ algos/DepthFirstOrder.java
@@ -4,11 +4,12 @@
 
 // imports
 import java.util.Stack;
+import java.util.Arrays;
 
 public class DepthFirstOrder
 {
   // DFS visit?
-  private boolean [] marked;
+  private Boolean [] marked;
   private int V;
   private Stack<Integer> reversePost = new Stack<Integer> ();
   
@@ -16,7 +17,7 @@ public class DepthFirstOrder
   {
     this.V = dg.V();
     
-    marked = new boolean[V];
+    marked = new Boolean[V];
     initialize(marked, false);
     
     for (int s=0; s<V; s++)
@@ -32,7 +33,7 @@ public class DepthFirstOrder
   
   private <T> void initialize(T[] a, T value)
   {
-    for (int i=0; i<a.length; a++)
+    for (int i=0; i<a.length;i++)
     {
       a[i] = value;
     }
@@ -56,6 +57,26 @@ public class DepthFirstOrder
   public Iterable<Integer> reversePostOrder()
   {
     return reversePost;
+  }
+
+
+  public static void main(String[] args) throws Exception
+  {
+    System.out.println("args = " + Arrays.toString(args));
+
+    DiGraph dg = new DiGraph(6);
+
+    dg.addEdge(5, 1);
+    dg.addEdge(1, 2);
+    dg.addEdge(2, 3);
+    dg.addEdge(1, 4);
+    dg.addEdge(4, 3);
+    dg.addEdge(0, 3);
+
+
+    DepthFirstOrder topo = new DepthFirstOrder(dg);
+    System.out.println(topo.reversePostOrder());
+
   }
 
 }
