ce3e92846ed04dc367dc469455dbd1fce61b2e32
r3t2 <ravitejac@gmail.com>
2017-01-15 13:00:09 -0800

    Create UF.java

---
+++ algos/UF.java
@@ -0,0 +1,14 @@
+/*
+* Class UF implements weighted quick union with path compression.
+* 
+* Constructor:
+* UF(int N) -- N is the number of 
+*/
+
+public class UF
+{
+  public UF(int N)
+  {
+    
+  }
+}
