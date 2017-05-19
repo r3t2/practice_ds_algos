f9f0e0b2f79b608ecf724ec4c2aa0186ddef33e2
r3t2 <ravitejac@gmail.com>
2017-05-19 07:26:19 -0700

    add main method. add findRoot using int[]

--- pi/ConstructTreeFromMatrix.java
+++ pi/ConstructTreeFromMatrix.java
@@ -1,6 +1,7 @@
 import java.util.List;
 import java.util.LinkedList;
 import java.util.Deque;
+import java.util.Arrays;
 
 public class ConstructTreeFromMatrix
 {
@@ -11,6 +12,8 @@ public class ConstructTreeFromMatrix
 	{
 		int N = mat[0].length;
 		Node [] adjList = new Node[N];
+		Integer parent[] = new Integer[N];
+		init(parent, -1);
 
 		for(int i=0; i<N; i++)
 		{
@@ -19,12 +22,31 @@ public class ConstructTreeFromMatrix
 				if(mat[i][j] == true)
 				{
 					addEdge(i, j, adjList);
+					parent[j] = i;
 				}
 			}
 		}
 
-		root = findRoot(adjList);
+		//root = findRoot(adjList);
+		root = adjList[findRoot(parent)];
 
+	}
+
+	private Integer findRoot(Integer[] parent)
+	{
+		int i = 0;
+
+		while(parent[i] == -1)
+		{
+			i++;
+		}
+
+		while(parent[i] != -1)
+		{
+			i = parent[i];
+		}
+
+		return i;
 	}
 
 	private Node findRoot(Node [] adjList)
@@ -82,6 +104,42 @@ public class ConstructTreeFromMatrix
 		{
 			arr[i] = val;
 		}
+	}
+
+	public List<Integer> preorder()
+	{
+		List<Integer> l = new LinkedList<Integer> ();
+		preorder(root, l);
+		return l;
+	}
+
+	private void preorder(Node x, List<Integer> l)
+	{
+		if(x == null)
+			return;
+
+		l.add(x.key);
+
+		for(Node y: x.siblings)
+		{
+			preorder(y, l);
+		}
+
+		return;
+	}
+
+	public static void main(String[] args)
+	{
+
+		boolean[][] mat = new boolean[][] {
+			{false, false, false, false,},
+			{false, false, false, false,},
+			{true,  true,  false, false,},
+			{false, false, true,  false,},
+		};
+
+		ConstructTreeFromMatrix t = new ConstructTreeFromMatrix(mat);
+		System.out.println(Arrays.toString( t.preorder().toArray()));
 	}
 
 	private class Node
