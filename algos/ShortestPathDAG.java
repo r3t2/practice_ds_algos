c75a7ddf806786aa3b9bc44b484ea1a29de864db
r3t2 <ravitejac@gmail.com>
2017-06-14 22:11:05 -0700

    add logic to detect cycle while computing topological order. Add main.

--- algos/ShortestPathDAG.java
+++ algos/ShortestPathDAG.java
@@ -1,6 +1,8 @@
 import java.util.Deque;
 import java.util.LinkedList;
-
+import java.util.Scanner;
+import java.util.HashSet;
+import java.util.Arrays;
 
 public class ShortestPathDAG
 {
@@ -12,7 +14,7 @@ public class ShortestPathDAG
   /*save reference to the graph being processed*/
   private EdgeWeightedDiGraph g;
 
-  public ShortestPathDAG(EdgeWeightedDiGraph g, int s)
+  public ShortestPathDAG(EdgeWeightedDiGraph g, int s) throws Exception
   {
     this.g = g;
     this.V = g.V();
@@ -23,10 +25,13 @@ public class ShortestPathDAG
     initArr(distTo, Double.POSITIVE_INFINITY);
     initArr(edgeTo, -1);
 
+    distTo[s] = 0;
+    edgeTo[s] = s;
+
     runSpDag(s);
   }
 
-  private void runSpDag(int s)
+  private void runSpDag(int s) throws Exception
   {
     Iterable<Integer> topo = topoOrder(s);
     for(Integer v : topo)
@@ -50,30 +55,41 @@ public class ShortestPathDAG
     }
   }
 
-  private Iterable<Integer> topoOrder(int s)
+  private Iterable<Integer> topoOrder(int s) throws Exception
   {
     boolean [] marked = new boolean[V];
     initArr(marked, false);
 
     Deque<Integer> dq = new LinkedList<Integer>();
-    topoOrder(marked, dq, s);
+    HashSet<Integer> inStack = new HashSet<Integer> ();
+
+    inStack.add(s);
+    topoOrder(marked, dq, inStack, s);
 
     return dq;
   }
 
-  private void topoOrder(boolean [] marked, Deque<Integer> dq, int v)
+  private void topoOrder(boolean [] marked, Deque<Integer> dq, HashSet inStack, int v) throws Exception
   {
     marked[v] = true;
     int w;
     for(DirectedEdge dE: g.adj(v))
     {
       w = dE.to();
+
+	  if(inStack.contains(w))
+	  {
+	    throw new Exception("cycle detected");
+	  }
+
       if(marked[w] == false)
       {
-        topoOrder(marked, dq, w);
+      	inStack.add(w);
+        topoOrder(marked, dq, inStack, w);
       }
     }
     dq.addFirst(v);
+    inStack.remove(v);
   }
 
   private void initArr(double [] arr, double val)
@@ -98,6 +114,27 @@ public class ShortestPathDAG
     {
       arr[i] = val;
     }
+  }
+
+  public String toString()
+  {
+  	StringBuilder sb = new StringBuilder();
+  	sb.append("distTo: " + Arrays.toString(distTo) + "\n");
+  	sb.append("edgeTo: " + Arrays.toString(edgeTo));
+
+  	return sb.toString();
+  }
+
+
+  public static void main(String[] args) throws Exception
+  {
+  	Scanner sc = new Scanner (System.in);
+  	EdgeWeightedDiGraph dg = new EdgeWeightedDiGraph(sc);
+  	System.out.println(dg);
+
+  	ShortestPathDAG sp = new ShortestPathDAG(dg, 0);
+
+  	System.out.println(sp);
   }
 
 
