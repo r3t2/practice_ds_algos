d59c5d334c85a281a3d6a5c3943184b39885c667
r3t2 <ravitejac@gmail.com>
2017-06-17 12:21:32 -0700

    add basic implementation of bellman ford algorithm

---
+++ algos/ShortestPathBellMan.java
@@ -0,0 +1,86 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class ShortestPathBellMan
+{
+	private Integer [] edgeTo;
+	private Double [] distTo;
+
+	private int V;
+	private EdgeWeightedDiGraph g;
+
+	public ShortestPathBellMan(EdgeWeightedDiGraph g, int s)
+	{
+		this.g = g;
+		this.V = this.g.V();
+		edgeTo = new Integer[V];
+		distTo = new Double[V];
+
+
+
+		init_arr(edgeTo, -1);
+		init_arr(distTo, Double.POSITIVE_INFINITY);
+
+
+		edgeTo[s] = s;
+		distTo[s] = 0.0;
+
+		runBellMan(s);
+
+	}
+	private void runBellMan(int s)
+	{
+		for (int i=0; i<V; i++)
+		{
+			for (int j=0; j<V; j++)
+			{
+				for (DirectedEdge e: g.adj(j))
+				{
+					relaxEdge(e);
+				}
+			}
+		}
+
+	}
+
+	private void relaxEdge(DirectedEdge e)
+	{
+		int v = e.from(), w = e.to();
+		double weight = e.weight();
+
+		if(distTo[v] + weight < distTo[w])
+		{
+			distTo[w] = distTo[v] + weight;
+			edgeTo[w] = v;
+		}
+	}
+
+	private <T> void init_arr(T[] arr, T val)
+	{
+		for (int i=0; i<arr.length; i++)
+		{
+			arr[i] = val;
+		}
+	}
+
+	public String toString()
+	{
+		StringBuilder sb = new StringBuilder();
+		sb.append("distTo = " + Arrays.toString(distTo) + "\n");
+		sb.append("edgeTo = " + Arrays.toString(edgeTo));
+
+		return sb.toString();
+	}
+
+	public static void main(String [] args)
+	{
+		Scanner sc = new Scanner(System.in);
+
+		EdgeWeightedDiGraph g = new EdgeWeightedDiGraph(sc);
+		System.out.println(g);
+
+		ShortestPathBellMan sp = new ShortestPathBellMan(g, 0);
+		System.out.println(sp);
+	}
+}
