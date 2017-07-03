302f0e669c1ddbf113f9b207e495c301fde14641
r3t2 <ravitejac@gmail.com>
2017-07-03 16:17:14 -0700

    add code to identify a cycle

--- algos/EulerTour.java
+++ algos/EulerTour.java
@@ -6,8 +6,7 @@ public class EulerTour
   /* get a copy of the graph to remove edges as we process them */
   private UnDirectedGraph g;
 
-  /* an outgoing edge from this vertex will be processed next*/
-  private int nextV;
+
 
   private int V;
   private int E;
@@ -25,17 +24,52 @@ public class EulerTour
 
     cycles = (Deque<Integer> []) new Deque[V];
 
-    nextV = 0;
-    while(nextV != -1)
+    /* an outgoing edge from this vertex will be processed next*/
+    int nextV = 0;
+    while(nextV < V)
     {
-      cycles[nextV] = getNextCycle();
+      cycles[nextV] = getNextCycle(nextV);
+      nextV = getNextV(nextV);
     }
 
 
   }
 
-  private Deque<Integer> getNextCycle()
+  private Deque<Integer> getNextCycle(int nextV)
   {
-    return new LinkedList<Integer>();
+    Deque<Integer> cycle = new LinkedList<Integer>();
+    Iterable<Integer> adj_v_itr;
+
+    int v = nextV, w;
+    adj_v_itr = g.adj(v);
+
+    while(adj_v_itr.hasNext())
+    {
+      w = adj_v_itr.next();
+      g.removeEdge(v, w);
+      cycle.add(w);
+
+      v = w;
+      adj_v_itr = g.adj(v);
+    }
+
+    if(v != nextV)
+    {
+      throw new Exception("cycle doesn't terminate at nextV = " + nextV);
+      //cycle = null;
+    }
+    return cycle;
+  }
+
+  private int getNextV(int v)
+  {
+    Iterable<Integer> adj_v_itr = g.adj(v);
+
+    while(!adj_v_itr.hasNext() && (v < V))
+    {
+      v = v+1;
+    }
+
+    return v;
   }
 }
