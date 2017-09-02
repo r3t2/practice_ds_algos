e9add1876add82a1233d2f01bd7843fce8ccd324
r3t2 <ravitejac@gmail.com>
2017-09-02 14:45:26 -0700

    fix blind-pivot binary search but corner cases remain

--- pi/RotatedSortedArray.java
+++ pi/RotatedSortedArray.java
@@ -75,8 +75,38 @@ public class RotatedSortedArray
 
 		int mid = (lo+hi)/2;
 
+    //System.out.println(String.format("lo = %d, mid = %d, hi = %d", lo, mid, hi));
+
 		if(x[mid] == searchKey)
 			return mid;
+
+    if(x[mid] < x[lo])
+    {
+      if( ((searchKey < x[mid]) && (searchKey < x[hi])) || ((searchKey > x[mid]) && (searchKey > x[hi])) )
+      {
+        return blindPivotBSearch(x, lo, mid-1, searchKey);
+      }
+      else
+      {
+        return blindPivotBSearch(x, mid+1, hi, searchKey);
+      }
+    }
+    else if (x[mid] >x[lo])
+    {
+      if(searchKey < x[mid] && searchKey > x[lo])
+      {
+        return blindPivotBSearch(x, lo, mid-1, searchKey); 
+      }
+      else
+      {
+        return blindPivotBSearch(x, mid+1, hi, searchKey); 
+      }
+    }
+    else /* x[mid] = x[lo]*/
+    {
+      
+    }
+
 	}
 
 	public int pivotBinarySearch(int [] x, int searchKey)
@@ -121,38 +151,50 @@ public class RotatedSortedArray
 		x = new int []{0,1,2,3,4,5};
 		runPivot(x);
 		runBinarySearch(x, 5);
+    runBlindPivotBSearch(x,5);
 
 		x = new int []{4,0,1,2,3};
 		runPivot(x);
 		runBinarySearch(x, 0);
+    runBlindPivotBSearch(x, 0);
 
 		x = new int []{1,2,3,4,0};
 		runPivot(x);
 		runBinarySearch(x, 5);
+    runBlindPivotBSearch(x, 5);
 
 		x = new int []{1,2,3,4,4,4,4,0};
 		runPivot(x);
 		runBinarySearch(x, 0);
+    runBlindPivotBSearch(x, 0);
 
 		x = new int []{4,4,4,4,4,4};
 		runPivot(x);
 		runBinarySearch(x, 4);
+    runBlindPivotBSearch(x, 4);
 
 		x = new int []{4,4,4,1,4,4};
 		runPivot(x);
 		runBinarySearch(x, 1);
+    runBlindPivotBSearch(x, 1);
 
 	}
 	private static void runPivot(int [] x)
 	{
 		RotatedSortedArray r = new RotatedSortedArray();
-		System.out.println("input = " + Arrays.toString(x));
+		System.out.println("\ninput = " + Arrays.toString(x));
 		System.out.println("pivot = " + r.pivot(x));	
 	}
 	private static void runBinarySearch(int [] x, int searchKey)
 	{
 		RotatedSortedArray r = new RotatedSortedArray();
-		System.out.println("input= " + Arrays.toString(x) + ", searchKey = " + searchKey);
+		System.out.println("searchKey = " + searchKey);
 		System.out.println("output index = " + r.pivotBinarySearch(x, searchKey));
 	}
+  private static void runBlindPivotBSearch(int [] x, int searchKey)
+  {
+    RotatedSortedArray r = new RotatedSortedArray();
+    System.out.println("searchKey = " + searchKey);
+    System.out.println("output index = " + r.blindPivotBSearch(x, searchKey));
+  }
 }
