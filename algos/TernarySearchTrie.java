d071ee5de7a9b2036487e96d8e5e48e170d40dac
r3t2 <ravitejac@gmail.com>
2017-09-04 10:49:41 -0700

    add prefixMatch method

--- algos/TernarySearchTrie.java
+++ algos/TernarySearchTrie.java
@@ -104,7 +104,43 @@ public class TernarySearchTrie<V>
     if(n.right != null) keys(n.right, sb, q, d);
   }
 
+  public boolean contains(String key)
+  {
+    return get(key) != null;
+  }
 
+  public Iterable<String> prefixMatch(String prefix)
+  {
+    Queue<String> prefixes = new LinkedList<String> ();
+
+    Node prefixNode = findNodeEndingAtPrefix(root, prefix, 0);
+
+    if(prefixNode != null)
+    {
+      if(prefixNode.val != null) prefixes.add(prefix);
+
+      StringBuilder sb = new StringBuilder(prefix);
+      keys(prefixNode.middle, sb, prefixes, prefix.length());
+    }
+
+    return prefixes;
+  }
+
+  private Node findNodeEndingAtPrefix(Node n, String prefix, int d)
+  {
+
+    if(n == null) return null;
+
+    char c = prefix.charAt(d);
+
+    if(c == n.c)
+    {
+      if(prefix.length() == (d+1)) return n;
+      else return findNodeEndingAtPrefix(n.middle, prefix, d+1);
+    }
+    else if(c < n.c) return findNodeEndingAtPrefix(n.left, prefix, d);
+    else return findNodeEndingAtPrefix(n.right, prefix, d);
+  }
 
   private class Node
   {
@@ -119,15 +155,28 @@ public class TernarySearchTrie<V>
 
     t.put("c", 1);
     t.put("cat", 2);
+    t.put("can", 2);
+    t.put("cans", 2);
     t.put("bat", 3);
     t.put("apple", 4);
     t.put("apple", 5);
+    t.put("dog", 5);
+    t.put("dogs", 6);
+    t.put("pizza", 6);
+    t.put("cbs", 7);
 
     System.out.println(t.get("c"));
     System.out.println(t.get("cat"));
+    System.out.println(t.get("cbs"));
     System.out.println(t.get("bat"));
     System.out.println(t.get("apple"));
+    System.out.println(t.get("dog"));
+    System.out.println(t.get("dogs"));
+    System.out.println(t.get("pizza"));
 
     System.out.println(t.keys());
+
+    System.out.println(t.prefixMatch("c"));
+    System.out.println(t.prefixMatch("d"));
   }
 }
