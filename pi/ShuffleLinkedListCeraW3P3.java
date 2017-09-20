dab07e2f0ee98eea4e515fb9f6d716a71fba5f06
r3t2 <ravitejac@gmail.com>
2017-09-19 19:16:03 -0700

    shuffle linkedlist

--- pi/ShuffleLinkedListCeraW3P3.java
+++ pi/ShuffleLinkedListCeraW3P3.java
@@ -1,4 +1,9 @@
-import java.util.Random;
+import java.util.Random; 
+/** 
+3.  Shuffling a linked list. Given a singly-linked list containing n
+items, rearrange the items uniformly at random. Your algorithm should consume a logarithmic (or
+constant) amount of extra memory and run in time proportional to nlogn in the worst case. 
+*/
 
 public class ShuffleLinkedListCeraW3P3<Item>
 {
