64466e905ee4426a5a3edf1b3e24fe0a83266acf
r3t2 <ravitejac@gmail.com>
2017-11-06 21:51:45 -0800

    add loop removal code

--- pi/gfg/top10/linkedLists/LinkedListLoopDetectAndRemove.java
+++ pi/gfg/top10/linkedLists/LinkedListLoopDetectAndRemove.java
@@ -15,6 +15,29 @@ public class LinkedListLoopDetectAndRemove
     return false;
   }
 
+  public static void detectAndRemoveLoop(Node head)
+  {
+    Node slow = head, fast = head;
+
+    while(slow != null && fast != null && fast.next !=null)
+    {
+      slow = slow.next;
+      fast = fast.next.next;
+      if(slow == fast) removeLoop(head, fast);
+    }
+
+  }
+
+  private static void removeLoop(Node slow, Node fast)
+  {
+    while(fast.next != slow.next)
+    {
+      fast = fast.next;
+      slow = slow.next;
+    }
+    fast.next = null;
+  }
+
   public static void main(String [] args)
   {
     runTest(new int[] {0, 1, 2, 3, 4, 5}, true);
@@ -38,15 +61,28 @@ public class LinkedListLoopDetectAndRemove
       last.next = temp;
     }
 
-    temp = head;
+    System.out.println("input LL = " + printLL(head, len));
+    System.out.println("has cycle = " + LinkedListLoopDetectAndRemove.detectLoop(head));
+    
+    System.out.println("removing cycle");
+    LinkedListLoopDetectAndRemove.detectAndRemoveLoop(head);
+    System.out.println("After removing cycle, detect cycle = " + LinkedListLoopDetectAndRemove.detectLoop(head));
+    System.out.println("Linked list after removing cycle = " + printLL(head, len));
+    System.out.printf("\n");
+  }
+
+  private static String printLL(Node head, int len)
+  {
+    Node temp = head;
+    StringBuffer sb = new StringBuffer();
+    sb.append("[");
     for(int i=0; i<2*len && temp!=null ; i++)
     {
-      System.out.printf("%d, ", temp.val);
+      sb.append(temp.val +", ");
       temp = temp.next;
     }
-    System.out.printf("\n");
-
-    System.out.println("has cycle = " + LinkedListLoopDetectAndRemove.detectLoop(head));
+    sb.append("]");
+    return sb.toString();
   }
   private static class Node
   {
