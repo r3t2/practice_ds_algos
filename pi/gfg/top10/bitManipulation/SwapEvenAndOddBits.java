93cfcd4a36a4e2c7351eb4b1746fe8daf3198f17
r3t2 <ravitejac@gmail.com>
2017-11-15 20:06:16 -0800

    Swap Even and Odd bits

--- pi/gfg/top10/bitManipulation/SwapEvenAndOddBits.java
+++ pi/gfg/top10/bitManipulation/SwapEvenAndOddBits.java
@@ -1,5 +1,10 @@
 /*
-
+wap all odd and even bits
+3.1
+Given an unsigned integer, swap all odd bits with even bits. 
+For example, if the given number is 23 (00010111), it should be converted to 43 (00101011). 
+Every even position bit is swapped with adjacent bit on right side (even position bits are 
+highlighted in binary representation of 23), and every odd position bit is swapped with adjacent on left side.
 */
 public class SwapEvenAndOddBits
 {
