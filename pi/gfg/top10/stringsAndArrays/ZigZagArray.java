232b9495e203ace913c3bd50f7ac5e7d4abb7b50
r3t2 <ravitejac@gmail.com>
2017-11-19 22:50:05 -0800

    zig zagify array

--- pi/gfg/top10/stringsAndArrays/ZigZagArray.java
+++ pi/gfg/top10/stringsAndArrays/ZigZagArray.java
@@ -1,5 +1,16 @@
 import java.util.Arrays;
+/*
+onvert array into Zig-Zag fashion
+2.4
+Given an array of distinct elements, rearrange the elements of array in zig-zag fashion in O(n) time. The converted array should be in form a < b > c < d > e < f.
 
+Example: 
+Input:  arr[] = {4, 3, 7, 8, 6, 2, 1}
+Output: arr[] = {3, 7, 4, 8, 2, 6, 1}
+
+Input:  arr[] =  {1, 4, 3, 2}
+Output: arr[] =  {1, 4, 2, 3}
+*/
 public class ZigZagArray
 {
     public static int [] zigZagify(int [] x)
