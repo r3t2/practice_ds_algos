209802784781fb64ec952e7d9b20d04cc7661758
r3t2 <ravitejac@gmail.com>
2017-11-23 11:50:35 -0800

    compute pow(x,y) iteratively. logN

--- pi/gfg/top10/numberTheory/ModularExponentiation.java
+++ pi/gfg/top10/numberTheory/ModularExponentiation.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Deque;
+
 public class ModularExponentiation
 {
     /* calculate x ** y.
@@ -5,8 +8,9 @@ public class ModularExponentiation
     public static int pow(int x, int y)
     {
         int res1 = powRecur(x, y);
-
-        return res1;
+        int res2 = powIter(x, y);
+        if(res1 != res2) System.out.printf("res1 = %d, res2 = %d\n", res1, res2);
+        return res2;
     }
 
     private static int powRecur(int x, int y)
@@ -17,6 +21,29 @@ public class ModularExponentiation
         int t = powRecur(x, y/2);
         if(y%2 == 0) return (t * t);
         else return (t * t) * x;
+    }
+
+    private static int powIter(int x, int y)
+    {
+        Deque<Integer> stack = new LinkedList<Integer>();
+        while(y!=0)
+        {
+            stack.addFirst(y);
+            y = y/2;
+        }
+
+        int prod = 1;
+        while(!stack.isEmpty())
+        {
+            y = stack.removeFirst();
+
+            if(y == 0) prod = 1; /* will not happen unless input y = 0*/
+            else if (y == 1) prod = x;
+            else if (y%2 == 0) prod = prod * prod;
+            else if (y%2 == 1) prod = prod * prod * x;
+        }
+
+        return prod;
     }
 
     public static void main(String [] args)
