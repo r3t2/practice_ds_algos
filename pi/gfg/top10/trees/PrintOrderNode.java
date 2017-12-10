9f9ca72b3c7f2e072ffb98a13fffcb4e09b8a7ce
r3t2 <ravitejac@gmail.com>
2017-12-09 17:00:02 -0800

    print a key along with level

---
+++ pi/gfg/top10/trees/PrintOrderNode.java
@@ -0,0 +1,15 @@
+public class PrintOrderNode
+{
+    public int k;
+    public int l;
+
+    public PrintOrderNode(int k, int l)
+    {
+        this.k = k;
+        this.l = l;
+    }
+    public String toString()
+    {
+        return "(" + k + ", " + l + ")";
+    }
+}
