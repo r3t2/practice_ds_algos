1997a9082ed4e5373c4fd3810dbecdba9a2f7559
r3t2 <ravitejac@gmail.com>
2018-01-03 23:53:10 -0800

    minimum window substring problem

--- pi/leetcode/MinWindowSubstring.java
+++ pi/leetcode/MinWindowSubstring.java
@@ -1,5 +1,6 @@
 /*
-iven a string S and a string T, find the minimum window in S which will contain all the characters in T in complexity O(n).
+76. Minimum Window Substring
+Given a string S and a string T, find the minimum window in S which will contain all the characters in T in complexity O(n).
 
 For example,
 S = "ADOBECODEBANC"
