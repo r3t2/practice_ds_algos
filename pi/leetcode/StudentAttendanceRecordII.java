82b74a36f32973c71ee9d150a1014c0318bd728f
r3t2 <ravitejac@gmail.com>
2018-02-06 17:27:15 -0800

    lg2 N implementation but MOD not working.

--- pi/leetcode/StudentAttendanceRecordII.java
+++ pi/leetcode/StudentAttendanceRecordII.java
@@ -9,7 +9,96 @@ public class StudentAttendanceRecordII
         {0, 0, 0, 1, 0, 0},
         {0, 0, 0, 0, 1, 0}
     };
+
     public static int checkRecord(int n)
+    {
+        if(n<1) return 0;
+        if(n==1) return 3;
+
+        int [][] matPowN = matrixModPow(transitionMatrix, n-1);
+        int [] sol = new int[] {1, 1, 0, 1, 0, 0};
+
+        sol = matrixMultMod(matPowN, sol);
+        int sum = 0;
+        for(int i=0; i<sol.length; i++)
+        {
+            sum = (sum + sol[i]) % MOD;
+            if(sum <= 0) sum = (sum + MOD) % MOD;
+        }
+
+        return sum;
+
+    }
+    private static int[][] matrixModPow(int [][] mat, int n)
+    {
+        if(n==1) return mat;
+
+        int [][] matNby2 = matrixModPow(mat, n/2);
+        int [][] matN = matrixMultMod(matNby2, matNby2);
+
+        //if n is odd
+        if(n%2 == 1)
+        {
+            matN = matrixMultMod(matN, mat);
+        }
+
+        return matN;
+    }
+    private static int [][] matrixMultMod(int [][] mat1, int [][] mat2)
+    {
+        int nR1 = mat1.length, nC1 = mat1[0].length;
+        int nR2 = mat2.length, nC2 = mat2[0].length;
+
+        if(nC1 != nR2) throw new IllegalArgumentException();
+
+
+        int [][] res = new int[nR1][nC2];
+        for(int iC2=0; iC2<nC2; iC2++)
+        {
+            for(int iR = 0; iR<nR1; iR++)
+            {
+                long sum = 0;
+                for(int iC1=0; iC1<nC1; iC1++)
+                {
+                    long mul = ((mat1[iR][iC1])*(mat2[iC1][iC2])) % MOD;
+                    if(mul <= 0) mul = (mul + MOD) % MOD;
+                    sum = (sum + mul) % MOD;
+                    if(sum<=0) sum = (sum + MOD) % MOD;
+                }
+                res[iR][iC2] = (int)sum;
+            }
+        }
+
+        return res;
+    }
+
+    private static int [] matrixMultMod(int [][] mat, int [] col)
+    {
+        int nR = mat.length, nC = mat[0].length;
+        if(nC != col.length) throw new IllegalArgumentException();
+
+        int [] ret = new int [nR];
+
+        for(int iR =0; iR<nR; iR++)
+        {
+            int sum=0;
+            for(int iC=0; iC<nC; iC++)
+            {
+                // long mul = (mat[iR][iC]*col[iC])%MOD;
+                // if(mul <=0) mul = (mul + MOD) % MOD;
+                sum = (sum + mat[iR][iC]*col[iC]) % MOD;
+                // sum = (sum + ((int)mul)) % MOD;
+                // if(sum <=0) sum = (sum + MOD)%MOD;
+            }
+            ret[iR] = sum;
+        }
+
+        return ret;
+    }
+
+
+
+    public static int checkRecordLinear(int n)
     {
         // for n=1:
         // dpCol: d_A_L_LL
@@ -33,38 +122,20 @@ public class StudentAttendanceRecordII
 
         return sum;
     }
-
-    private static int [] matrixMultMod(int [][] mat, int [] col)
-    {
-        int nR = mat.length, nC = mat[0].length;
-        if(nC != col.length) throw new IllegalArgumentException();
-
-        int [] ret = new int [nR];
-
-        for(int iR =0; iR<nR; iR++)
-        {
-            int sum=0;
-            for(int iC=0; iC<nC; iC++)
-            {
-                sum = (sum + mat[iR][iC]*col[iC]) % MOD;
-            }
-            ret[iR] = sum;
-        }
-
-        return ret;
-    }
-
     public static void main(String [] args)
     {
         runTest(1);
         runTest(2);
         runTest(3);
         runTest(4);
+        runTest(25);
+        runTest(50);
+        runTest(128);
         runTest(100);
     }
 
     private static void runTest(int n)
     {
-        System.out.printf("n = %5d, numWays = %5d\n", n, checkRecord(n));
+        System.out.printf("n = %5d, numWays = %10d, numWaysLin = %10d\n", n, checkRecord(n), checkRecordLinear(n));
     }
 }
